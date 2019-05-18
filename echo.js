4caf7929e2a8abde85ba49813f73e1a135fe0eb4
Josep <josep.arpi@gmail.com>
2019-05-18 13:02:28 +0200

    Get post body

--- echo.js
+++ echo.js
@@ -1,3 +1,5 @@
-module.exports = (req, res) => {
-  res.end("hello!"+ req.body);
+const { json  } = require('micro')
+module.exports = async (req, res) => {
+  const body = await json(req)
+  res.end("hello!"+ body.username);
 }
