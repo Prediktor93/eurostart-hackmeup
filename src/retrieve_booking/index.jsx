2eb0752b926f6cc76246d0efd0ef41b56989b837
Josep <josep.arpi@gmail.com>
2019-05-18 18:35:55 +0200

    Fix relative url

--- src/retrieve_booking/index.jsx
+++ src/retrieve_booking/index.jsx
@@ -3,17 +3,18 @@ import React from 'react'
 class RetrieveBookingForm extends React.Component {
     constructor(props) {
       super(props)
+      this.state = {booking:{}};
     }
 
-    getBooking() {
+    async getBooking() {
       const bookingNumber = document.getElementById('bnumber').value
       const lastName = document.getElementById('lname').value
       const isValidBookingNumber = /[A-z]{6}/.test(bookingNumber);
       if (!isValidBookingNumber) {
         // ...
       }
-      const url = "http://localhost:3000/retrieve?booking=" + bookingNumber + "&name=" + lastName;
-      return fetch(url, {
+      const url = "retrieve?booking=" + bookingNumber + "&name=" + lastName;
+      const data = await fetch(url, {
       method: 'GET',
       headers: {
           'Content-Type': 'application/json',
@@ -29,6 +30,8 @@ class RetrieveBookingForm extends React.Component {
       .catch(function (err) {
           console.dir(err)
       })
+      console.dir(data);
+      this.setState({booking: data});
     }
 
     render() {
@@ -36,7 +39,8 @@ class RetrieveBookingForm extends React.Component {
         <div id="retrieveBookingContainer">
             <input placeholder="Booking number" name="bnumber" id="bnumber" type="text"></input>
             <input placeholder="Last name" name="lname" id="lname" type="text"></input>
-            <button id="retrieve_booking" onClick={this.getBooking}>Search</button>
+            <button id="retrieve_booking" onClick={() => this.getBooking()}>Search</button>
+            <div>{JSON.stringify(this.state.booking)}</div>
         </div>)
     }
 }
