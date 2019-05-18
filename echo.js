4a8ade7e5f04963ec76a4cb98195a5f7a7da585f
Adrian <adrian.martinez.requena@gmail.com>
2019-05-18 15:43:02 +0200

    post de prueba a echo

--- echo.js
+++ echo.js
@@ -1,5 +1,9 @@
+const url = require('url')
+
 const { json  } = require('micro')
 module.exports = async (req, res) => {
-  const body = await json(req)
-  res.end("hello!"+ body.username);
+  //const body = await json(req)
+  let { query } = url.parse(req.url, true)
+  console.dir(query)
+  res.end("hello! " + query.from);
 }
