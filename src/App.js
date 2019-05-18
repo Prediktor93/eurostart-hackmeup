85ac6c9bc3834d6e5f105e69e7b30f6b41798654
AGamboaM <andymanu1992@gmail.com>
2019-05-18 17:54:39 +0200

    Create tabs for main page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import TrainSearchForm from './search_components/index.jsx';
-import HotelSearchForm from './hotel_search/index.jsx';
+import HotelSearchForm from './hotel_search/index.jsx'
 import RetrieveBookingForm from './retrieve_booking/index.jsx';
 import './App.css';
 
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import "react-tabs/style/react-tabs.css";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <TrainSearchForm></TrainSearchForm>
-        <HotelSearchForm></HotelSearchForm>
-        <RetrieveBookingForm></RetrieveBookingForm>
-      </header>
+    <div>
+      <Tabs>
+        <TabList>
+          <Tab>Train Search</Tab>
+          <Tab>Hotel Search</Tab>
+          <Tab>Manage Booking</Tab>
+        </TabList>
+
+        <TabPanel>
+          <TrainSearchForm></TrainSearchForm>
+        </TabPanel>
+        <TabPanel>
+          <HotelSearchForm></HotelSearchForm>
+        </TabPanel>
+        <TabPanel>
+          <RetrieveBookingForm></RetrieveBookingForm>
+        </TabPanel>
+      </Tabs>
     </div>
   );
 }
